b7cb493e964fb512936a1efcae52d71b6d54eb97
munir79 <jakirhossainmunir79@gmail.com>
2023-08-09 10:34:08 +0600

    data read from database

--- Index.js
+++ Index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_PASSWORD}@cluster0.9b6ho97.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+//console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -39,9 +39,23 @@ async function run() {
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  const ServiceCollection=client.db("GeniusCar").collection("Services");
+
+  // read data from Database 
+  app.get('/services',async(req,res)=>{
+    const query={};  
+    const cursor=ServiceCollection.find(query);
+    const services=await cursor.toArray(); // convert array all data
+    res.send(services); 
+  })
+
+
+
+
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
